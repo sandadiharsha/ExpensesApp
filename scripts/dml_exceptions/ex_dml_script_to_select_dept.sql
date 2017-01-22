b37a439394612fd31fb3f9de2ebc5b7e4813e04d
sandadiharsha <sandadi.harsha@gmail.com>
2017-01-22 22:38:28 +0530

    EX_1 https//crucipble.com/CEX_2

--- scripts/dml_exceptions/ex_dml_script_to_select_dept.sql
+++ scripts/dml_exceptions/ex_dml_script_to_select_dept.sql
@@ -4,7 +4,9 @@ Submitted Date: 21/01/2017
 Submitted By: N. Narendra
 Description: Script to query all values from Dept table
 */
+declare
+ln_count number;
 begin
-select * from DEPT;
+select count(1) into ln_count from DEPT;
 commit;
 end;
