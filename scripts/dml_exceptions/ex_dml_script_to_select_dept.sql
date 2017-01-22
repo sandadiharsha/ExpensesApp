966c476f37461735d85635a3dfd2d3dd1f8de8cc
sandadiharsha <sandadi.harsha@gmail.com>
2017-01-22 22:36:14 +0530

    EX_1 https//crucipble.com/CEX_1

---
+++ scripts/dml_exceptions/ex_dml_script_to_select_dept.sql
@@ -0,0 +1,10 @@
+/*
+Project: Expeses APP
+Submitted Date: 21/01/2017
+Submitted By: N. Narendra
+Description: Script to query all values from Dept table
+*/
+begin
+select * from DEPT;
+commit;
+end;
